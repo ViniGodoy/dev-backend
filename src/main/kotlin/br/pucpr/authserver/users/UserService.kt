2ed2c2f421a1a0b64252fa600a69f1daf0ded6ab
Vinícius Mendonça <vinicius.mendonca@wwt.com>
2023-07-09 10:44:02 -0300

    Projeto inicial da aula 04 contendo o código finalizado da aula 03

--- src/main/kotlin/br/pucpr/authserver/users/UserService.kt
+++ src/main/kotlin/br/pucpr/authserver/users/UserService.kt
@@ -1,10 +1,23 @@
 package br.pucpr.authserver.users
 
+import br.pucpr.authserver.exception.BadRequestException
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
 class UserService(val repository: UserRepository) {
-    fun insert(user: User) = repository.save(user)
-    fun findAll() = repository.findAll()
+    fun insert(user: User): User {
+        if (repository.findByEmailOrNull(user.email) != null) {
+            throw BadRequestException("User already exists")
+        }
+        return repository.save(user)
+    }
+
+    fun findAll(dir: SortDir = SortDir.ASC) = repository.findAll(dir)
     fun findByIdOrNull(id: Long) = repository.findByIdOrNull(id)
+    fun delete(id: Long): Boolean {
+        val user = repository.findByIdOrNull(id) ?: return false
+        repository.delete(user)
+        return true
+    }
 }
---
+++ src/main/kotlin/br/pucpr/authserver/users/controller/UserController.kt
@@ -0,0 +1,44 @@
+package br.pucpr.authserver.users.controller
+
+import br.pucpr.authserver.users.SortDir
+import br.pucpr.authserver.users.UserService
+import br.pucpr.authserver.users.controller.requests.CreateUserRequest
+import br.pucpr.authserver.users.controller.responses.UserResponse
+import jakarta.validation.Valid
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/users")
+class UserController(val service: UserService) {
+    @PostMapping
+    fun insert(@Valid @RequestBody user: CreateUserRequest) =
+        UserResponse(service.insert(user.toUser()))
+            .let { ResponseEntity.status(HttpStatus.CREATED).body(it) }
+
+    @GetMapping
+    fun list(@RequestParam sortDir: String?) =
+        service.findAll(SortDir.findOrThrow(sortDir ?: "ASC"))
+            .map { UserResponse(it) }
+            .let { ResponseEntity.ok(it) }
+
+
+    @GetMapping("/{id}")
+    fun getById(@PathVariable id: Long) =
+        service.findByIdOrNull(id)
+            ?.let { ResponseEntity.ok(UserResponse(it)) }
+            ?: ResponseEntity.notFound().build()
+
+    @DeleteMapping("/{id}")
+    fun delete(@PathVariable id: Long): ResponseEntity<Void> =
+        if (service.delete(id)) ResponseEntity.ok().build()
+        else ResponseEntity.notFound().build()
+}
